73d17a06dad4796d5cd76ee00a4ad9c9807acc42
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-05-28 16:55:27 -0400

    Solutions to the loop problems

--- src/CovidStats.java
+++ src/CovidStats.java
@@ -28,13 +28,55 @@ public class CovidStats {
             int dailyIncrease = covid19[i] - previousDayValue;
             dailyCaseCount[i] = dailyIncrease;
             previousDayValue = covid19[i];
-            System.out.println(dailyIncrease);
         }
 
-        for(int n: dailyCaseCount) {
-            System.out.println(n);
+        int sum = 0;
+        for( int n : dailyCaseCount) {
+            sum = sum + n;
         }
 
+        double average = (double)sum/(double)dailyCaseCount.length;
+        System.out.printf("The daily average is %.2f\n",average);
+
+        int totalCount = covid19[covid19.length - 1];
+        int halfCount = totalCount / 2;
+
+        int halfCountIndex = -1;
+        int i = 0;
+        while ( i < covid19.length && halfCountIndex == -1) {
+            if(covid19[i] > halfCount) {
+                halfCountIndex = i - 1;
+            }
+            i = i + 1;
+        }
+
+        int numDaysSinceDouble = (covid19.length - 1) - halfCountIndex;
+
+        System.out.printf("The number of days since the number of cases doubled is: %d\n", numDaysSinceDouble);
+
+        int largest = Integer.MIN_VALUE;
+        int largestIndex = -1;
+        for(int j = 0; j < dailyCaseCount.length; j = j + 1) {
+            if(dailyCaseCount[j] > largest) {
+                largest = dailyCaseCount[j];
+                largestIndex = j;
+            }
+        }
+
+        System.out.printf("The largest daily increase of %d happened on day %d\n", largest, largestIndex);
+
+        int weekNum = 1;
+        int sevenDaySum = 0;
+        for (int k = 0; k < dailyCaseCount.length; k = k + 1){
+            if(k % 7 == 0) {
+                if(k != 0) {
+                    System.out.printf("Average daily increase for week %d = %.2f\n", weekNum, ((double)sevenDaySum/7.0));
+                    weekNum = weekNum + 1;
+                }
+                sevenDaySum = 0;
+            }
+            sevenDaySum = sevenDaySum + dailyCaseCount[k];
+        }
 
     }
 }
